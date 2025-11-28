f6f796d1f20f463a64ce8e3fbf94c8e9e9137a0b
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-11-28 10:23:19 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const VALID_COMMANDS: [&str;1] = ["echo"];
+
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Failed to read line");
+    let input = buffer.trim();
+    if input.is_empty() {
+        return;
+    }
+
+    match shlex::split(input) {
+        Some(args) => {
+            for arg in args {
+                if !VALID_COMMANDS.contains(&arg.as_str()){
+                    print!("{}: command not found", arg);
+                }
+            }
+        },
+        None => {}
+    }
 }
