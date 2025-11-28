a8fdadd0cf4105b57778586a791b296ca5d8067b
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-11-28 22:42:37 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
+use bytes::{BufMut, BytesMut};
+use log::debug;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-const VALID_COMMANDS: [&str;1] = ["echo"];
+const VALID_COMMANDS: [&str; 1] = ["echo"];
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    env_logger::init();
 
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).expect("Failed to read line");
-    let input = buffer.trim();
-    if input.is_empty() {
-        return;
-    }
+    while true {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read line");
+        let input = buffer.trim();
+        if input.is_empty() {
+            return;
+        }
 
-    match shlex::split(input) {
-        Some(args) => {
-            for arg in args {
-                if !VALID_COMMANDS.contains(&arg.as_str()){
-                    print!("{}: command not found", arg);
+        debug!("input: {}", input);
+
+        match shlex::split(input) {
+            Some(args) => {
+                debug!("args: {:?}", args);
+                for arg in args {
+                    if !VALID_COMMANDS.contains(&arg.as_str()) {
+                        println!("{}: command not found", arg);
+                    }
                 }
             }
-        },
-        None => {}
+            None => {}
+        }
+        buffer.clear();
     }
 }
