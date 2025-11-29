e13427529ed43aadbf1a5831d15c81391c7371b0
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-11-29 21:18:32 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use anyhow::Result;
 use log::debug;
+use std::env;
+use std::fs;
+use std::process::ExitCode;
 
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
 
+const BUILTINS: [&str; 3] = ["echo", "exit", "type"];
 const EXIT_COMMAND: &str = "exit";
 
+#[derive(Debug)]
 enum Command {
     Exit(u8),
     Echo(Vec<String>),
@@ -21,32 +27,69 @@ fn handle_command(cmd: &Command) -> (bool, u8) {
             (true, 1)
         }
         Command::Type(value) => {
-            let cmd = parse_command(value, &[]);
-            if cmd.is_none() && !value.is_empty() {
-                println!("{}: not found", value);
-            } else if !cmd.is_none() {
+            if BUILTINS.contains(&value.as_str()) {
                 println!("{} is a shell builtin", value);
+                return (true, 1);
+            }
+            let bin_path = find_exec(value);
+            debug!("bin_path {:?}", bin_path);
+            if bin_path.is_empty() {
+                println!("{}: not found", value);
+            } else {
+                println!("{} is {}", value, bin_path);
             }
             (true, 1)
         }
-        _ => (true, 1),
     }
 }
+fn find_exec(binary: &String) -> String {
+    let paths = env::var("PATH").unwrap_or_default();
+    for path in env::split_paths(&paths) {
+        debug!(
+            "Searching for binary {} in {}",
+            binary,
+            path.to_str().unwrap()
+        );
+        if !path.exists() {
+            continue;
+        }
+        for entry in fs::read_dir(path).expect("Failed to read directory") {
+            let entry = entry.expect("Failed to read directory entry");
+            let path = entry.path();
+            let path_name = path.to_str().expect("Failed to convert path to string");
+            if !path.is_file() {
+                continue;
+            }
+            let file_name = entry.file_name();
+            debug!("testing path {:?} with file_name {:?}", path, file_name);
+            if file_name.to_str().expect("Failed to get filename") == binary {
+                let metadata = fs::metadata(&path).expect("Failed to get metadata");
+                debug!(
+                    "{:0o} metadata permissions",
+                    metadata.permissions().mode() & 0o111
+                );
 
+                if metadata.permissions().mode() & 0o111 != 0 {
+                    return String::from(path_name);
+                }
+            }
+        }
+    }
+
+    String::from("")
+}
 fn parse_command(input: &str, args: &[String]) -> Option<Command> {
     match input {
         EXIT_COMMAND => Some(Command::Exit(0)),
         val => match val {
             "echo" => Some(Command::Echo(Vec::from(args))),
-            "type" => Some(Command::Type(String::from(
-                args.first().unwrap_or(&"".to_string()),
-            ))),
+            "type" => Some(Command::Type(args.first()?.clone())),
             _ => None,
         },
     }
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<ExitCode> {
     env_logger::init();
 
     let mut buffer = String::new();
@@ -56,7 +99,7 @@ fn main() -> Result<()> {
     while next {
         // Read: Display a prompt and wait for user input
         print!("$ ");
-        io::stdout().flush().unwrap();
+        io::stdout().flush()?;
         io::stdin()
             .read_line(&mut buffer)
             .expect("Failed to read line");
@@ -79,6 +122,7 @@ fn main() -> Result<()> {
                 }
             }
             Some(cmd) => {
+                debug!("{:?}", cmd);
                 (next, return_code) = handle_command(&cmd);
             }
         }
@@ -89,5 +133,5 @@ fn main() -> Result<()> {
         //Loop again
     }
     debug!("return_code: {}", return_code);
-    Ok(())
+    Ok(ExitCode::from(return_code))
 }
